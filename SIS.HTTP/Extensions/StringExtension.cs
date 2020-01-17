010f79e3f4f34f30f6f0a8c57f46fad552fe223f
Vasil Velichkov <vasko_v@abv.bg>
2020-01-17 08:46:23 +0200

    first part

---
+++ SIS.HTTP/Extensions/StringExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace SIS.HTTP.Extensions
+{
+    public static class StringExtension
+    {
+        public static string Capitalize(this string text)
+        {
+            string result = char.ToUpper(text[0]) + text.Substring(1).ToLower();
+            return result;
+        }
+    }
+}
---
+++ SIS.HTTP/Requests/HttpRequest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SIS.HTTP.Common;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Exceptions;
+using SIS.HTTP.Headers;
+using SIS.HTTP.Headers.Contracts;
+using SIS.HTTP.Requests.Contracts;
+
+namespace SIS.HTTP.Requests
+{
+    public class HttpRequest : IHttpRequest
+    {
+        public HttpRequest(string requestString)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(requestString, nameof(requestString));
+
+            this.FormData = new Dictionary<string, object>();
+            this.QueryData = new Dictionary<string, object>();
+            this.Headers = new HttpHeaderCollection();
+
+            this.ParseRequest(requestString);
+        }
+
+        public string Path { get; private set; }
+        public string Url { get; private set; }
+        public Dictionary<string, object> FormData { get; }
+        public Dictionary<string, object> QueryData { get; }
+        public IHttpHeaderCollection Headers { get; }
+        public HttpRequestMethod RequestMethod { get; private set; }
+
+        private bool IsValidRequestLine(string[] requestLine)
+        {
+            return requestLine.Length == 3 && requestLine[2] == GlobalConstants.HttpOneProtocolFragment;
+        }
+
+        private bool IsValidRequestQuearyString(string queryString, string[] queryParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParseRequestMethod(string[] requestLine)
+        {
+            HttpRequestMethod method;
+            bool parseResult = HttpRequestMethod.TryParse(requestLine[0], out method);
+
+            if (!parseResult)
+            {
+                throw new BadRequestException(string.Format(GlobalConstants.UnsupportedHttpMethodExceptionMessage, requestLine[0]));
+            }
+
+            this.RequestMethod = method;
+        }
+
+        private void ParseRequestUrl(string[] requestLine)
+        {
+
+        }
+
+        private void ParseRequestPath()
+        {
+
+        }
+
+        private void ParseHeaders(string[] requestLine)
+        {
+
+        }
+
+        private void ParseCookies()
+        {
+
+        }
+
+        private void ParseQueryParameters(string formData)
+        {
+
+        }
+
+        private void ParseFormDataParameters(string formData)
+        {
+
+        }
+
+        private void ParseRequestParameters(string requestString)
+        {
+
+        }
+
+        private void ParseRequest(string requestString)
+        {
+            string[] splitRequestContent = requestString
+                .Split(new[]{GlobalConstants.HttpNewLine}, StringSplitOptions.None);
+
+            string[] requestLine =
+                splitRequestContent[0].Trim().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (!this.IsValidRequestLine(requestLine))
+            {
+                throw new BadRequestException();
+            }
+
+            this.ParseRequestMethod(requestLine);
+            this.ParseRequestUrl(requestLine);
+            this.ParseRequestPath();
+
+            this.ParseHeaders(splitRequestContent.Skip(1).ToArray());
+            //this.ParseCookies();
+
+            this.ParseRequestParameters(splitRequestContent[splitRequestContent.Length -1]);
+        }
+
+    }
+}
